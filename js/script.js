58b0d128c2608bc5efa9077b3d91baec75dd93a9
Lolloc99 <lolloc99@yahoo.it>
2022-04-11 14:58:43 +0200

    Bonus 1 concluso

--- js/script.js
+++ js/script.js
@@ -16,23 +16,26 @@ Consigli del giorno:
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 */
 
+const rowWrapper = document.querySelector(".row");
+
 // Stampare in console i numeri da 1 a 100
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     // Var standard
     let num = i;
 
     if ((i % 3 === 0) && (i % 5 === 0)) {
         // Per i numeri che sono sia multipli di 3 che di 5 stampa FizzBuzz.
-        num = "FizzBuzz"
+        num = "fizzbuzz"
     } else if (i % 3 === 0) {
         //  Per i multipli di 3 stampa Fizz
-        num = "Fizz";
+        num = "fizz";
     } else if  (i % 5 === 0){
         //  Per i multipli di 5 stampa Buzz
-        num = "Buzz";
+        num = "buzz";
     }
 
     console.log(num);
+    rowWrapper.innerHTML += `<div class="box ${num}">${num}</div>`;
 
 }
 
