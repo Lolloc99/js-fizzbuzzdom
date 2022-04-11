7a580738d4dbbd93071eff8ef7a497bd62eac81a
Lolloc99 <lolloc99@yahoo.it>
2022-04-11 14:23:38 +0200

    Esercizio base concluso, mancanti bonus

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@ Consigli del giorno:
 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 */
+
+// Stampare in console i numeri da 1 a 100
+for (let i = 0; i <= 100; i++) {
+    // Var standard
+    let num = i;
+
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        // Per i numeri che sono sia multipli di 3 che di 5 stampa FizzBuzz.
+        num = "FizzBuzz"
+    } else if (i % 3 === 0) {
+        //  Per i multipli di 3 stampa Fizz
+        num = "Fizz";
+    } else if  (i % 5 === 0){
+        //  Per i multipli di 5 stampa Buzz
+        num = "Buzz";
+    }
+
+    console.log(num);
+
+}
+
+
